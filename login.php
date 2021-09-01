219e393e873a458505603490a0ff3cd89bdbadd3
crysthel <62146267+crysthel@users.noreply.github.com>
2021-09-01 17:27:19 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<?php
+
+session_start();
+$errors = array();
+
+  include('db.php');
+
+  if (isset($_POST['loginBtn'])) {
+		$username = mysqli_real_escape_string($conn, $_POST['userName']);
+		$password =  mysqli_real_escape_string($conn, $_POST['password']);
+
+		//ensure that form fields are filled properly
+		if(empty($username)){
+		 array_push($errors, "Username is required");
+		}
+		else if(empty($password)){
+      array_push($errors, "Password is required");
+		}
+
+		if (count($errors) == 0) {
+			$password = md5($password); // encrypt before comparing with that from database
+			$query = "SELECT * FROM login WHERE Username = '$username' AND Password='$password'";
+			$result = mysqli_query($conn, $query);
+			if (mysqli_num_rows($result) == 1) {
+				// log user in
+          $_SESSION['username'] = $username;
+  				$_SESSION['success'] = "You are now logged in";
+  				header('location: Home.php'); // redirect to homepage
+  			}else{
+          array_push($errors, "wrong  username/password combination");
+  			}
+		}
+
+
+	}
+
+
+
+ ?>
+
 <html>
   <head>
     <title>Login</title>
@@ -15,6 +55,7 @@
             <img src="img/avatar.svg" class="avatar" />
               <h1 class="login">Login</h1>
                 <form action="login.php" method="post">
+                  <?php include('errors.php'); ?>
                   <label>Username</label><br />
                   <input type="text" placeholder="Enter username" name="userName" /> <br /><br />
                   <label>Password</label> <br />
@@ -29,49 +70,9 @@
 
 
 
-<?php
 
-  session_start();
-	$errors = array();
-
-  include('server.php');
-
-  if (isset($_POST['loginBtn'])) {
-		$username = mysqli_real_escape_string($conn, $_POST['userName']);
-		$password =  mysqli_real_escape_string($conn, $_POST['password']);
-
-		//ensure that form fields are filled properly
-		if(empty($username)){
-		  array_push($errors, "Username is required");
-
-		}
-		if(empty($password)){
-			array_push($errors, "Password is required");
-		}
-
-		if (count($errors) == 0) {
-			$password = md5($password); // encrypt before comparing with that from database
-			$query = "SELECT * FROM login WHERE Username = '$username' AND Password='$password'";
-			$result = mysqli_query($conn, $query);
-			if (mysqli_num_rows($result) == 1) {
-				// log user in
-          $_SESSION['username'] = $username;
-  				$_SESSION['success'] = "You are now logged in";
-  				header('location: Home.php'); // redirect to homepage
-  			}else{
-  				array_push($errors, "wrong  username/password combination");
-  			}
-		}
-
-
-	}
-
-
-
- ?>
 
 
   </body>
 
 </html>
-
